a262a845f43f117c55ba7f8436cefc18517b80a3
CEITEC Itapipoca <culturamakerprogramacaoeia@gmail.com>
2026-06-25 10:48:42 -0300

    fix: adiciona UsuarioCreationForm ao forms.py do corretor

--- plataforma/corretor/core/forms.py
+++ plataforma/corretor/core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
 from .models import Avaliacao, Resultado
 
 
@@ -113,3 +114,9 @@ class CorrecaoForm(forms.Form):
         max_length=50,
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Turma'})
     )
+
+
+class UsuarioCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'first_name', 'last_name')
